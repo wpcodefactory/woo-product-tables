8ef07b9d8a96777e965bc6a2fd52c8ed19e8bb17
Istiaq Nirab <40653908+Ok9xNirab@users.noreply.github.com>
2026-07-08 08:14:14 +0600

    Fixed Found usage of constant "PLUGINDIR". Use WP_PLUGIN_DIR instead.

--- classes/logger.php
+++ classes/logger.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * Product Table by WBW - LoggerWtbp class.
+ *
+ * @version 2.3.0
+ */
+
 define('WTBP_LOG', true);
 
 class LoggerWtbp {
@@ -16,13 +22,23 @@ class LoggerWtbp {
 		return self::getInstance();
 	}
 
+	/**
+	 * log.
+	 *
+	 * @version 2.3.0
+	 *
+	 * @param $message
+	 * @param $data
+	 *
+	 * @return void
+	 */
 	public function log( $message, $data = '' ) {
 		if ( defined( 'WTBP_LOG' ) && WTBP_LOG === true ) {
 			if ( ! is_string( $data ) && ! is_numeric( $data ) ) {
 				$data = var_export( $data, true );
 			}
 			if ( ! function_exists( 'wc_get_logger' ) ) {
-				include_once( ABSPATH . PLUGINDIR . '/woocommerce/woocommerce.php' );
+				include_once( WP_PLUGIN_DIR . '/woocommerce/woocommerce.php' );
 			}
 			wc_get_logger()->debug( "{$message} \n\n {$data} \n", array( '_legacy' => true ) );
 		}
